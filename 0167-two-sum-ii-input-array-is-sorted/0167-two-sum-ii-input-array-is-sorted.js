884b59359d295a2f79e73ada9a17a667ccdfcb53
William Ngo <105512356+williamkimngo@users.noreply.github.com>
2023-02-06 17:32:57 -0800

    Time: 70 ms (64.64%), Space: 43.2 MB (32.21%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let start = 0
+    let end = numbers.length - 1
+    
+    while (start <= end) {
+        if (numbers[start] + numbers[end] === target) {
+            return [start + 1, end + 1]
+        } else if (numbers[start] + numbers[end] > target) {
+            end--
+        } else {
+            start++
+        }
+    }
+    return []
+};
