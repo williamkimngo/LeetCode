1c3ac7d9b65d7404284ea90b71177aae15fe817a
William Ngo <105512356+williamkimngo@users.noreply.github.com>
2023-03-07 14:27:49 -0800

    Time: 89 ms (11.63%), Space: 47.3 MB (5.32%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.js
+++ 1029-two-city-scheduling/1029-two-city-scheduling.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 var twoCitySchedCost = function(costs) {
-let h = costs.length/2;
-    let output = 0;
+let half = costs.length/2;
+    let total = 0;
 
     //sort by difference of each pair and descending order.
     costs.sort((a,b)=>(b[1]-b[0])-(a[1]-a[0]));
-
-    for (let i=0;i<h;i++) {
-        output += costs[i][0];
+    console.log(costs)
+    for (let i=0;i<half;i++) {
+        total += costs[i][0];
     }
 
-    for (let i=h;i<costs.length;i++) {
-        output += costs[i][1];
+    for (let i=half;i<costs.length;i++) {
+        total += costs[i][1];
     }
 
-    return output;
+    return total;
 };
