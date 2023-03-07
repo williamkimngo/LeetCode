1bdc35194651a71dcc27df5248e1a0f66dd7efff
William Ngo <105512356+williamkimngo@users.noreply.github.com>
2023-03-06 18:26:26 -0800

    Time: 186 ms (56.07%), Space: 65.2 MB (47.59%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.js
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.js
@@ -10,19 +10,40 @@
  * @param {TreeNode} root
  * @return {number}
  */
+
+//BFS
+// var deepestLeavesSum = function(root) {
+//     let q = [root], ans, qlen, curr
+//     while (q.length) {
+//         qlen = q.length, ans = 0
+//         for (let i = 0; i < qlen; i++) {
+//             curr = q.shift(), ans += curr.val
+//             if (curr.left) {
+//                 q.push(curr.left)
+//             }
+//             if (curr.right) {
+//                 q.push(curr.right)
+//             }
+//         }
+//     }
+//     return ans
+// };
+
 var deepestLeavesSum = function(root) {
-    let q = [root], ans, qlen, curr
-    while (q.length) {
-        qlen = q.length, ans = 0
-        for (let i = 0; i < qlen; i++) {
-            curr = q.shift(), ans += curr.val
-            if (curr.left) {
-                q.push(curr.left)
-            }
-            if (curr.right) {
-                q.push(curr.right)
-            }
+    let sums = []
+    const dfs = (node, lvl) => {
+        if (lvl === sums.length) {
+            sums[lvl] = node.val
+        } else {
+            sums[lvl] += node.val
+        }
+        if (node.left) {
+            dfs(node.left, lvl + 1)
+        }
+        if(node.right) {
+            dfs(node.right, lvl + 1)
         }
     }
-    return ans
-};
+    dfs(root, 0)
+    return sums[sums.length - 1]
+}
