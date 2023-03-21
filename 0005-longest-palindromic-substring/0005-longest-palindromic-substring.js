0c916cc9b6919f6831b57a80964d96ada4c19783
William Ngo <105512356+williamkimngo@users.noreply.github.com>
2023-03-21 07:56:46 -0700

    Time: 482 ms (34.72%), Space: 47.8 MB (47.41%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  for (let j = s.length - 1; j >= 0; j--) {
+    let i = 0,
+      k = j;
+    while (k < s.length) {
+      let substr = s.substring(i, k + 1);
+      if (isPalindrome(substr)) return substr;
+      i++, k++;
+    }
+  }
+  return "";
+};
+
+function isPalindrome(str) {
+  let l = 0,
+    r = str.length - 1;
+  while (l < r) {
+    if (str[l] !== str[r]) return false;
+    l++, r--;
+  }
+  return true;
+}
