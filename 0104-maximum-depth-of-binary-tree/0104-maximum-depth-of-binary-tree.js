b7c724903b29556c1180a63f71f0cd122891858b
William Ngo <105512356+williamkimngo@users.noreply.github.com>
2023-04-12 17:59:59 -0700

    Time: 75 ms (34.56%), Space: 45.3 MB (34.23%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) return 0;
+    const queue = [root];
+    let depth = 0;
+    while (queue.length !== 0) {
+        depth++;
+        const len = queue.length;
+        for (let i = 0; i < len; i++) {
+            if (queue[i].left) queue.push(queue[i].left);
+            if (queue[i].right) queue.push(queue[i].right);
+        }
+        queue.splice(0, len);
+    }
+    return depth;
+};
